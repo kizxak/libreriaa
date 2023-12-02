8cb3a4860e2e20ad5440426a4cc15aa07a17e9e4
Edelflow <130091834+Edelflow@users.noreply.github.com>
2023-12-02 17:23:54 -0300

    views.py

--- libros/views.py
+++ libros/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def listar_libros(request):
+    libros = Libros.objects.all()
+    return render(request, "libros/listar_libros.html", {"libros": libros})
