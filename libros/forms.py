f71c9a27ed6fa42a384bfffd6783e44c6c853033
kizxak <emilia.branje@hotmail.com>
2023-12-05 17:43:46 -0300

    forms

--- libros/forms.py
+++ libros/forms.py
@@ -1,2 +1,14 @@
 from django import forms
-import models
+import .models import Libros , Socios
+from django.forms import TextInput,DateField
+
+class ProductoForm(forms.ModelForm):
+    class Meta:
+        model = Producto
+        fields = ['titulo','autor','editorial','fecha_publicacion']
+        widgets ={
+            'titulo' : TextInput(attrs={'class': "form-control",'placeholder': 'Título'}),
+            'autor':TextInput(attrs={'class': "form-control",'placeholder': 'Autor'}),
+            'editorial':TextInput(attrs={'class': "form-control",'placeholder': 'Editorial'}),
+            'fecha_publicación':DateInput(attrs={'class': "form-control",'placeholder': 'Precio'}),
+        }
