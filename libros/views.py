064d3d47f05ea6e362d1e52a7cce03e006946cc4
kizxak <kizxak231@gmail.com>
2023-12-09 15:43:26 -0300

    lista

--- libros/views.py
+++ libros/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Libros
+
 
 # Create your views here.
 def listar_libros(request):
     libros = Libros.objects.all()
-    return render(request, "libros/listar_libros.html", {"libros": libros})
+    return render(request, "listar_libros.html", {"libros": libros})
