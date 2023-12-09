76e218db1a1f7f11bf60024d1040b64a580b8857
DavidSandovalInacap <138266985+DavidSandovalInacap@users.noreply.github.com>
2023-12-09 19:23:08 -0300

    views.py

--- libros/views.py
+++ libros/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import Libros
 
@@ -7,3 +7,14 @@ from .models import Libros
 def listar_libros(request):
     libros = Libros.objects.all()
     return render(request, "listar_libros.html", {"libros": libros})
+
+def eliminar_libro(request):
+    if 'id' in request.GET:
+        id_libro = request.GET['id']
+        libro = get_object_or_404(Libros, id_libro=id_libro)
+
+        # Verifica si el libro existe antes de intentar eliminarlo
+        if libro:
+            libro.delete()
+
+    return redirect("listar-libros")
